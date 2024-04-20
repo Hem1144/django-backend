39560049e70305b9ea589a1f0af473c39f01e377
Hem1144 <hemlaldulal7@gmail.com>
2024-04-20 19:38:15 +0545

    feat: Applied some template engines of Django

--- home/views.py
+++ home/views.py
@@ -3,7 +3,24 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "index.html")
+
+    pepoles = [
+        {"name": "Hemlal", "age": 24},
+        {"name": "Ram", "age": 40},
+        {"name": "Demo", "age": 34},
+        {"name": "Sandeep", "age": 31},
+        {"name": "Pradeep", "age": 13},
+    ]
+
+    vegetables = ["Pumpkin", "Tomato", "Cauliflower", "Cabbage"]
+
+    text = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ullam impedit totam! Atque, debitis officiis amet sed veritatis voluptatibus. Voluptate, sed impedit quae suscipit magni cumque eum optio, dignissimos dicta cupiditate tenetur corrupti sequi amet dolorum earum soluta pariatur nemo aliquam nobis laboriosam explicabo, distinctio provident. A sunt voluptatem quaerat hic rem quisquam reprehenderit quod quo repellendus, veritatis, earum dignissimos voluptatum quasi eos minima provident omnis quas. Qui tempora facere suscipit asperiores earum est distinctio tenetur, quam dignissimos eum porro veniam expedita sequi voluptatem! Expedita velit, necessitatibus libero sed, fugit et molestias ipsum iusto, totam rerum maiores. Amet, provident eaque."""
+
+    return render(
+        request,
+        "index.html",
+        context={"peoples": pepoles, "text": text, "vegetables": vegetables},
+    )
 
 
 def success_page(request):
